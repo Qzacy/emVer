fd835071b85d3001474a08271dacfc5fe2921357
Qzacyer <47668628+Qzacy@users.noreply.github.com>
2019-11-04 18:39:20 +0100

    Update emver.py

--- emver.py
+++ emver.py
@@ -9,7 +9,7 @@ if len(sys.argv) != 2:
     print("Usage:\npython3 emver.py [test@mail.com]")
     sys.exit()
 mail = sys.argv[1]
-api_key = "at_ILs0jQmYIThKqwypzUkOXdYgZeoj1"
+api_key = ""
 data = requests.get("https://emailverification.whoisxmlapi.com/api/v1?apiKey=" + api_key + "&emailAddress=" + mail)
 resp = data.json()
 
